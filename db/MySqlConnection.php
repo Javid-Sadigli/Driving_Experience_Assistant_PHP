b41da2a924793c3b0994c7083e1222eb564a6a93
Javid Sadigli <cavidoffical@gmail.com>
2024-12-05 13:16:05 +0400

    Deleted unused code piece

--- db/MySqlConnection.php
+++ db/MySqlConnection.php
@@ -7,24 +7,6 @@
         public function __construct(
             string $host, string $username, string $password, string $db
         ){
-            // mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-            // try
-            // {
-            //     $this->mysqli = new mysqli($host, $username, $password, $db); 
-
-            //     if ($this->mysqli->connect_errno)
-            //     {
-            //         die("Connection error: " . $this->mysqli->connect_errno);
-            //     }
-            // }
-            // catch(Exception $e)
-            // {
-            //     echo "MySQL Error Code : " . $e->getCode();
-            //     echo "<br>";
-            //     echo "Exception message : " . $e->getMessage();
-            //     exit();
-            // }
-
             $dsn="mysql:host" . $host . ";dbname=" . $db . ";charset=utf8mb4";
 
             try 
