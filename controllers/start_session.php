f178e6d61b6e8e3275a4b91c87f706b6eccf3131
Javid Sadigli <cavidoffical@gmail.com>
2024-12-09 13:27:53 +0400

    Updated start_session.php

--- controllers/start_session.php
+++ controllers/start_session.php
@@ -3,8 +3,6 @@ include('../util/random_pw.php');
 
 session_start(); 
 $_SESSION['code'] = array();
-$code = random_pw(20);
-$_SESSION['code'][$code] = 0;
 
 header('Location: ../views/templates/index.php');
 exit; 
