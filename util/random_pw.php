5447320626d9240c34854c5fb6ad7803f625aa12
Javid Sadigli <cavidoffical@gmail.com>
2024-12-23 12:13:19 +0400

    Fixed deprecated code

--- util/random_pw.php
+++ util/random_pw.php
@@ -1,13 +1,17 @@
 <?php
+
     function random_pw($pw_length) 
     {
-        $pass = NULL;
+        $pass = '';
         $charlist = 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz023456789';
         $ps_len = strlen($charlist);
-        mt_srand((double)microtime()*1000000);
-
-        for($i = 0; $i < $pw_length; $i++) {
+    
+        mt_srand((int)((microtime(true) * 1000000) % PHP_INT_MAX));
+    
+        for ($i = 0; $i < $pw_length; $i++) {
             $pass .= $charlist[mt_rand(0, $ps_len - 1)];
         }
-        return ($pass);
+    
+        return $pass;
     }
+    
